c6395306eb53ecf8440d705664c8a2af78bd0a74
vikasexcel <vikas.excel2011@gmail.com>
2026-01-14 21:36:03 +0530

    chore: update download links to v0.1.9

--- bettershot-landing/lib/downloads.ts
+++ bettershot-landing/lib/downloads.ts
@@ -1,7 +1,7 @@
 export const downloadLinks = {
   appleSilicon:
-    "https://github.com/KartikLabhshetwar/better-shot/releases/download/v0.1.8/bettershot_0.1.8_aarch64.dmg",
+    "https://github.com/KartikLabhshetwar/better-shot/releases/download/v0.1.9/bettershot_0.1.9_aarch64.dmg",
   intel:
-    "https://github.com/KartikLabhshetwar/better-shot/releases/download/v0.1.8/bettershot_0.1.8_x64.dmg",
+    "https://github.com/KartikLabhshetwar/better-shot/releases/download/v0.1.9/bettershot_0.1.9_x64.dmg",
 } as const
 
